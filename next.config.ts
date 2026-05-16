de2c86e74dd6758fe2c7cc9f435b3ea7189c77e4
walterh1996 <walterhindman1996@gmail.com>
2026-05-15 21:53:26 -0500

    auto-backup: 2026-05-15 21:53:26

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from "next";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-const nextConfig: NextConfig = {};
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
+};
 
 export default nextConfig;
