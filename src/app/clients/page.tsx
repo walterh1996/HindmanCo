a1aae4c06b759be78d57c5cf34df605ecdc3d6ce
walterh1996 <walterhindman1996@gmail.com>
2026-06-30 15:43:40 -0500

    auto-backup: 2026-06-30 15:43:40

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -1,5 +1,6 @@
 import { FadeIn } from "@/components/fade-in";
 import Image from "next/image";
+import Link from "next/link";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
